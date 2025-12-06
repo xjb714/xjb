93351b986d1f62ca7ff4e29d62970a3889e4c103
xjb <1302035400@qq.com>
2025-12-07 00:55:50 +0800

    update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,7 +5,7 @@
 #include "bench/xjb/dtoa_xjb64_xjb32.cpp" // full table
 
 
-int main() 
+int main()
 {
     double v[]={
         123.45,
@@ -44,17 +44,27 @@ int main()
         printf("i = %d, xjb64_full : %10s  xjb32_full : %10s\n",i,buf_full_double,buf_full_float);
         printf("i = %d, xjb64_comp : %10s  xjb32_comp : %10s\n",i,buf_comp_double,buf_comp_float);
     }
-    int i=0;
+    int i = 0;
     while(i++ < 30)
     {
         printf("input a real number:");
         double num;
         scanf("%le",&num);
-        xjb64_32::xjb64(num,buf_full_double);
-        xjb64_32_comp::xjb64(num,buf_comp_double);
-        xjb64_32::xjb32(num,buf_full_float);
-        xjb64_32_comp::xjb32(num,buf_comp_float);
-        printf("xjb64_full : %10s  xjb32_full : %10s\n",buf_full_double,buf_full_float);
-        printf("xjb64_comp : %10s  xjb32_comp : %10s\n",buf_comp_double,buf_comp_float);
+
+        char* buf_end_full_double = xjb64_32::xjb64(num,buf_full_double);
+        char* buf_end_comp_double = xjb64_32_comp::xjb64(num,buf_comp_double);
+        char* buf_end_full_float = xjb64_32::xjb32(num,buf_full_float);
+        char* buf_end_comp_float = xjb64_32_comp::xjb32(num,buf_comp_float);
+
+        int write_length_full_double = buf_end_full_double - buf_full_double;
+        int write_length_comp_double = buf_end_comp_double - buf_comp_double;
+        int write_length_full_float = buf_end_full_float - buf_full_float;
+        int write_length_comp_float = buf_end_comp_float - buf_comp_float;
+
+
+        printf("xjb64_full : %10s write length = %2d\n",buf_full_double,write_length_full_double);
+        printf("xjb64_comp : %10s write length = %2d\n",buf_comp_double,write_length_comp_double);
+        printf("xjb32_full : %10s write length = %2d\n",buf_full_float,write_length_full_float);
+        printf("xjb32_comp : %10s write length = %2d\n",buf_comp_float,write_length_comp_float);
     }
 }
