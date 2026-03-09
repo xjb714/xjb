0fdaef2a1a56327a4fd0933d4e7797bb1fb30f75
xjb714 <1302035400@qq.com>
2026-03-09 10:44:22 +0800

    fix uscalec interface error

--- bench/uscalec/uscalec_i.hpp
+++ bench/uscalec/uscalec_i.hpp
@@ -12,5 +12,5 @@ char* uscalec_f64_to_str(double value, char* buffer) {
     return uscalec::uscale_short(value,buffer);
 }
 static inline void uscalec_f64_to_dec(double v, unsigned long long *dec, int *e10){
-    uscalec::Short(v, dec, e10);
+    uscalec::Short(v, (uint64_t*)dec, e10);
 }
